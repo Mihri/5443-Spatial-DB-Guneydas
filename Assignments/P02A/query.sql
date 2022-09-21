66aedf13361004582ee0f6eb71b53fba0bd9e298
Mihri <mihribanguneydas@gmail.com>
2022-09-20 22:18:33 -0500

    Create query.sql

---
+++ Assignments/P02A/query.sql
@@ -0,0 +1,19 @@
+--query for primary roads
+SELECT *,geom::json FROM p_roads
+LIMIT 5;
+
+--query for rails
+SELECT *,geom::json FROM rails
+LIMIT 5;
+
+--query for states
+SELECT *,geom::json FROM states
+LIMIT 5;
+
+--query for mil
+SELECT *,geom::json FROM mil
+LIMIT 5;
+
+--query for zone
+SELECT *,geom::json FROM zone
+LIMIT 5;
